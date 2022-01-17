a67246212f9cfed163eee534cce6a35a59c9611a
Kenny <mccormick84@gmail.com>
2022-01-17 19:54:52 +0900

    프로젝트 생성 Context API 실습

---
+++ screens/FeedsScreen.js
@@ -0,0 +1,33 @@
+import React, {useContext} from 'react';
+import {StyleSheet, View, TextInput} from 'react-native';
+import LogContext from '../contexts/LogContext';
+
+function FeedsScreen() {
+  const {text, setText} = useContext(LogContext);
+  return (
+    <View style={styles.block}>
+      <TextInput
+        value={text}
+        onChangeText={setText}
+        placeholder={'텍스트를 입력하세요'}
+        style={styles.input}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  block: {},
+  box: {
+    borderWidth: 2,
+    padding: 16,
+    borderBottomColor: 'black',
+    marginBottom: 16,
+  },
+  input: {
+    padding: 16,
+    backgroundColor: 'white',
+  },
+});
+
+export default FeedsScreen;
