0cfd5fc31632904721d895127d1d9ca763c274be
Kenny <mccormick84@gmail.com>
2022-01-19 18:50:11 +0900

    애니매이션 적용 (Animated)

--- screens/FeedsScreen.js
+++ screens/FeedsScreen.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import FloatingWriteButton from '../components/FloatingWriteButton';
 import LogContext from '../contexts/LogContext';
@@ -6,12 +6,17 @@ import FeedList from '../components/FeedList';
 
 function FeedsScreen() {
   const {logs} = useContext(LogContext);
-  // console.log(JSON.stringify(logs, null, 2));
+  const [hidden, setHidden] = useState(false);
+  const onScrolledToBottom = isBottom => {
+    if (hidden !== isBottom) {
+      setHidden(isBottom);
+    }
+  };
 
   return (
     <View style={styles.block}>
-      <FeedList logs={logs} />
-      <FloatingWriteButton />
+      <FeedList logs={logs} onScrolledToBottom={onScrolledToBottom} />
+      <FloatingWriteButton hidden={hidden} />
     </View>
   );
 }
