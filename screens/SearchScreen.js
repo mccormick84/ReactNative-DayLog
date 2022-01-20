1c35f5282f82c02babc5bff2d5deac0bfcb59971
Kenny <mccormick84@gmail.com>
2022-01-20 18:14:56 +0900

    검색 기능 구현

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {StyleSheet, View} from 'react-native';
+import EmptySearchResult from '../components/EmptySearchResult';
+import SearchContext from '../contexts/SearchContext';
+import FeedList from '../components/FeedList';
+import LogContext from '../contexts/LogContext';
 
-function SearchScreen() {
-  return <View style={styles.block} />;
+function SearchScreen({navigation}) {
+  const {keyword} = useContext(SearchContext);
+  const {logs} = useContext(LogContext);
+
+  const filtered =
+    keyword === ''
+      ? []
+      : logs.filter(log =>
+          [log.title, log.body].some(text => text.includes(keyword)),
+        );
+  if (keyword === '') {
+    return <EmptySearchResult type={'EMPTY_KEYWORD'} />;
+  }
+
+  if (filtered.length === 0) {
+    return <EmptySearchResult type={'NOT_FOUND'} />;
+  }
+
+  return (
+    <View style={styles.block}>
+      <FeedList logs={filtered} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  block: {},
+  block: {
+    flex: 1,
+  },
 });
 
 export default SearchScreen;
